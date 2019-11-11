baa408165f839c2cd98bec02183eed8048f08628
Nicole Sano <nicole.honyaku@gmail.com>
2019-11-11 17:26:10 +0900

    Added mostly Harry Potter seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Make 10 users
+10.times do
+  user = User.new(email: Faker::Internet.email, password: Faker::DrivingLicence.uk_driving_licence, first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, address: Faker::Nation.capital_city)
+  user.save!
+end
+
+# Make 20 packages, 2 for each user
+User.all.each do |user|
+  2.times do
+   package = Package.new(user: user, name: Faker::Movies::HarryPotter.spell, price: (1..10).to_a.sample, origin: Faker::Nation.flag, description: Faker::Movies::HarryPotter.quote)
+   package.save!
+  end
+end
+
+# Make 60 items, 3 for each package
+
+Package.all.each do |package|
+  (3..6).to_a.sample.times do
+    item = Item.new(package: package, name: Faker::Food.ingredient)
+    item.save!
+  end
+end
