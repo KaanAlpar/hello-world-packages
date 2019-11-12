2e9cafda3bdb386c63d690bf60838f9f5d0a6ff6
Kaan Alpar <kaanalpar33@gmail.com>
2019-11-12 14:44:27 +0900

    fix various bugs'

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,21 +1,17 @@
 class OrdersController < ApplicationController
-  # def new
-  #   @order = Order.new
-  # end
 
   def create
     @order = Order.new(order_params) # strong params!(?)
     if @order.save
       redirect_to user_path(current_user)
     else
-      # redirect_to new_order_path
-      render  # new.html.erb
+      # try again
     end
   end
 
   private
 
-  # def order_params
-  #   params.require(:order).permit(:)
-  # end
+  def order_params
+    params.require(:order).permit(:user_id, :package_id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
-  get 'orders/new'
-  get 'orders/create'
   devise_for :users
   root to: 'pages#home'
   get '/dashboard', to: 'users#show', as: 'dashboard'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
   resources :packages, only: [:index, :show, :new, :create] do
     resources :orders, only: [:create]
   end
