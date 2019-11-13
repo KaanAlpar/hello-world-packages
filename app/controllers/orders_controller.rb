42d27b5daaf4b016528e53df7fd26b1679185904
Kaan Alpar <kaanalpar33@gmail.com>
2019-11-13 14:46:09 +0900

    show orders showing orders. back button returns to dashboard after ordering a package

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,12 +1,16 @@
 class OrdersController < ApplicationController
   def create
-    @order = Order.new(order_params) # strong params!(?)
-    if @order.save
-      redirect_to user_path(current_user)
-    else
-      # try again
-      redirect_to package_path(params[:id])
-    end
+    @order = Order.new # strong params!(?)
+    @order.user = current_user
+    @order.package = Package.find(params[:package_id])
+    @order.save!
+
+    # if @order.save
+    #   redirect_to user_path(current_user)
+    # else
+    #   # try again
+    #   redirect_to package_path(params[:id])
+    # end
   end
 
   private
