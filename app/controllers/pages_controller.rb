1f012a64d3d50880bfe649b888c7a7328577b688
Kaan Alpar <kaanalpar33@gmail.com>
2019-11-13 13:33:29 +0900

    styled result package card

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,6 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
 
   def home
-    @countries = Package.select(:origin).distinct.pluck(:origin)
+    @countries = Package.select(:origin).distinct.pluck(:origin).sort
   end
 end
