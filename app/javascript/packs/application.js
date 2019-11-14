16ecfc378a619281b34d3bfcf41cd29a56f9bb44
Kaan Alpar <kaanalpar33@gmail.com>
2019-11-14 22:27:22 +0900

    algolia working, just need to display the results on the page html

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,3 +4,18 @@ import 'select2/dist/css/select2.css';
 import { initSelect2 } from '../components/init_select2';
 
 initSelect2();
+
+document.getElementById('search_countries').addEventListener('keyup', (e) => {
+  var client = algoliasearch("EZ854SMHEA", "dd68a0c2da477b9cf380a5a7dca05afa");
+  var index = client.initIndex('Package');
+  index.search(e.currentTarget.value, { hitsPerPage: 10, page: 0 })
+    .then(function searchDone(content) {
+      content.hits.forEach(function(item) {
+        console.log(item.name);
+      });
+      console.log('---------------------------------')
+    })
+    .catch(function searchFailure(err) {
+      console.error(err);
+    });
+});
