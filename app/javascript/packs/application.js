0eccca10e25fbdc432f4fa4e862686de7f6955d0
Kaan Alpar <kaanalpar33@gmail.com>
2019-11-15 11:32:03 +0900

    search working but needs styling

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,15 +5,17 @@ import { initSelect2 } from '../components/init_select2';
 
 initSelect2();
 
+let resultsList = document.getElementById('results');
 document.getElementById('search_countries').addEventListener('keyup', (e) => {
   var client = algoliasearch("EZ854SMHEA", "dd68a0c2da477b9cf380a5a7dca05afa");
-  var index = client.initIndex('Package');
+  var index = client.initIndex('Country');
   index.search(e.currentTarget.value, { hitsPerPage: 10, page: 0 })
     .then(function searchDone(content) {
+      resultsList.innerHTML = "";
       content.hits.forEach(function(item) {
-        console.log(item.name);
+        const listItem = `<li class="results-list-item">${item.name}</li>`;
+        resultsList.insertAdjacentHTML('beforeend', listItem);
       });
-      console.log('---------------------------------')
     })
     .catch(function searchFailure(err) {
       console.error(err);
