40956d8ce88a3d2de483911188756d1d02c087b2
Nicole Sano <nicole.honyaku@gmail.com>
2019-11-15 15:11:17 +0900

    Fixed navbar issue

--- app/javascript/components/navbar.js
+++ app/javascript/components/navbar.js
@@ -1,11 +1,11 @@
 const initUpdateNavbarOnScroll = () => {
-  const navbar = document.querySelector('.navbar-lewagon');
+  const navbar = document.querySelector('.navbar-lewagon.transparentable');
   if (navbar) {
     window.addEventListener('scroll', () => {
-      if (window.scrollY >= 500) {
-        navbar.classList.add('navbar-lewagon-white');
+      if (window.scrollY >= 200) {
+        navbar.classList.remove('transparent');
       } else {
-        navbar.classList.remove('navbar-lewagon-white');
+        navbar.classList.add('transparent');
       }
     });
   }
