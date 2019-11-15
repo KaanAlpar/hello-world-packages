81a26874f585ccce308473cb799a5603cd2666f1
Kaan Alpar <kaanalpar33@gmail.com>
2019-11-15 11:55:30 +0900

    styling mostly done

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,16 +6,19 @@ import { initSelect2 } from '../components/init_select2';
 initSelect2();
 
 let resultsList = document.getElementById('results');
-document.getElementById('search_countries').addEventListener('keyup', (e) => {
+let searchInput = document.getElementById('search_countries');
+searchInput.addEventListener('keyup', (e) => {
   var client = algoliasearch("EZ854SMHEA", "dd68a0c2da477b9cf380a5a7dca05afa");
   var index = client.initIndex('Country');
   index.search(e.currentTarget.value, { hitsPerPage: 10, page: 0 })
     .then(function searchDone(content) {
       resultsList.innerHTML = "";
-      content.hits.forEach(function(item) {
-        const listItem = `<li class="results-list-item">${item.name}</li>`;
-        resultsList.insertAdjacentHTML('beforeend', listItem);
-      });
+      if (searchInput.value !== "") {
+        content.hits.forEach(function(item) {
+          const listItem = `<a href="/packages?utf8=✓&search%5Bcountries%5D=${item.name}&commit=Search"><li class="results-list-item">${item.name}</li></a>`;
+          resultsList.insertAdjacentHTML('beforeend', listItem);
+        });
+      }
     })
     .catch(function searchFailure(err) {
       console.error(err);
